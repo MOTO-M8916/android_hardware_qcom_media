96e06061f075c5c27ca3d621a99b8bc379c2f76b
Vijay Venkatraman <vijaykv@google.com>
2017-06-07 11:31:20 -0700

    Add libutils to msm8996 modules
    These dependencies are explicitly needed when enabling VNDK flag
    
    Bug: 33241851
    Test: build target and pass CTS
    Test: With BOARD_VNDK_VERSION, mma works
    Change-Id: I662e4f698e5206e6a713b878c380cdaf9b50ef07

--- mm-video-v4l2/vidc/vdec/Android.mk
+++ mm-video-v4l2/vidc/vdec/Android.mk
@@ -185,7 +185,7 @@ LOCAL_C_INCLUDES              += $(libmm-vdec-inc)
 LOCAL_ADDITIONAL_DEPENDENCIES := $(libmm-vdec-add-dep)
 
 LOCAL_PRELINK_MODULE          := false
-LOCAL_SHARED_LIBRARIES        := liblog libcutils
+LOCAL_SHARED_LIBRARIES        := liblog libcutils libutils
 LOCAL_SHARED_LIBRARIES        += libswvdec
 
 LOCAL_SRC_FILES               := src/omx_swvdec.cpp
